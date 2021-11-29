ae5156083f2438d2f5f9717adc1904994aeade1d
IdanLnrd <87257934+IdanLnrd@users.noreply.github.com>
2021-11-29 10:15:07 +0200

    adding server

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-const { execSync } = require("child_process");
+
+require('dotenv').config();
+const { 
+    execSync 
+} = require("child_process");
+const express = require('express');
+const cors = require('cors');
+const { readFileSync, existsSync } = require('fs');
+const {
+    PORT,
+    PUBLIC_DIR
+} = process.env;
 
 function getParsedResume(filename) {
     const cvCommand = `python3 ${__dirname}/main.py '${filename}'`
@@ -6,5 +17,30 @@ function getParsedResume(filename) {
     return (result.toString())
 }
 
-// getParsedResume(`${__dirname}/../data/1Amy.pdf`);
+const app = express();
+app.use(cors());
+app.use(express.static(PUBLIC_DIR));
 
+app.get('/list/cv', (req, res) => {
+    return [
+        
+    ];
+});
+
+app.get('/parse/cv/:path', (req, res) => {
+    const path = req.params.path;
+    const exists = existsSync(path);
+    let result = null;
+    if(!exists) {
+        return res.json({ err: 'file not exists', result, input: path })
+    }
+    result = getParsedResume(path);
+    return res.json({ err: '', result, input: path });
+});
+
+app.listen(PORT, 
+    () => console.log(`server running on port: ${PORT}`)
+);
+
+// getParsedResume(`${__dirname}/../data/1Amy.pdf`);
+ 
