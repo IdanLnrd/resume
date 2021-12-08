cb74c7c081278bb656c6af7f355a7260db957056
IdanLnrd <87257934+IdanLnrd@users.noreply.github.com>
2021-12-08 09:18:18 +0200

    clean data script

--- generate_cv_clean_text.js
+++ generate_cv_clean_text.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-const { readdirSync, rmdirSync, existsSync, writeFileSync, mkdirSync, rmSync } = require('fs');
+const { readdirSync, rmdirSync, existsSync, writeFileSync, mkdirSync, rmSync, readFileSync } = require('fs');
 const { resolve , extname } = require('path');
 const { CV_DIR, CLEAN_CV_DIR } = process.env;
-
+function cleanText(text) {
+    return  text.replace(/•+|!+/igu, '').replace(/\s+/mgiu, ' ').toLowerCase();
+}
 const prepare = () => {
     const raw = resolve(__dirname, CV_DIR);
     const clean = resolve(__dirname, CLEAN_CV_DIR);
@@ -17,22 +19,50 @@ const prepare = () => {
     return { raw, clean };
 }
 
-const ext = [
-    '.pdf',
-    '.doc',
-    '.docx',
-    '.txt'
-];
+const EXT = {
+    pdf: '.pdf',
+    doc: '.doc',
+    docx: '.docx',
+    txt: '.txt'
+}
 
-(async () => {
+const extensions = Object.values(EXT);
+
+const PDF = '.pdf';
+
+const run = async () => {
     const { raw, clean } = prepare();
     if(!raw || !clean) {
         return console.log('missing path:', { raw, clean });
     }
-    const cvpaths = readdirSync(raw).map(r => resolve(raw, r));
-    const invalidFiles = cvpaths.filter(
-        p => !ext.includes(extname(p))
-    );
-    invalidFiles.forEach(f => rmSync(f));
-    console.log(invalidFiles);
-})();
+    const cvpaths = readdirSync(raw).map(r => resolve(raw, r)).filter(
+        p => extensions.includes(extname(p))
+    )
+    const pdf = require('./extractors/PDFExtractor');
+    const doc = require('./extractors/DOCEctractor');
+    let text, ext;
+    let counter = 0;
+    const n = String(cvpaths.length).length;
+    for(const cv of cvpaths) {
+        ext = extname(cv);
+        if(ext === EXT.txt) {
+            text = cleanText(readFileSync(cv).toString());
+        }
+        if(ext === EXT.pdf) {
+            text = cleanText(await pdf.getText(cv));
+        }
+        if(ext === EXT.docx || ext === EXT.doc) {
+            text = cleanText(await doc.getText(cv));
+        }
+        if(text) {
+            const newpath = resolve(clean, `${String(counter).padStart(n, 0)}${EXT.txt}`)
+            writeFileSync(newpath);
+            counter++;
+        }
+     
+    }
+
+
+
+
+};
